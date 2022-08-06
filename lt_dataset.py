9d9f7c3811708afead74712a753868ce54e9e298
saeed-khorram <khorram.saeed@gmail.com>
2022-08-06 16:51:12 -0700

    lt dataset added

---
+++ lt_dataset.py
@@ -0,0 +1,41 @@
+import json
+import argparse
+import numpy as np
+import os
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='LongTail Dataset')
+    parser.add_argument('--fn', metavar='PATH', type=str, help='path to the dataset json file')
+    parser.add_argument('--lt_cls', type=int, help='index of the lt class')
+    parser.add_argument('--lt_count', type=int, help='number of samples in the lt class')
+    args = parser.parse_args()
+
+    pwd = os.os.path.dirname(args.fn)
+
+    with open(args.fn) as f:
+        main = json.load(f)["labels"]
+
+    if main:
+        main = np.array(main, dtype=object)
+    else:
+        exit(0)
+
+    all_inds = np.arange(main.shape[0])
+    lt_class_inds = np.where(main[:, 1] == args.lt_cls)[0]
+    s_inds = lt_class_inds[:args.lt_count]
+    ns_inds = lt_class_inds[args.lt_count:]
+    # s_lt_inds = np.random.choice(lt_inds, size=args.lt_count, replace=False)
+    # ns_lt_inds = np.array(list(set(lt_inds) - set(s_lt_inds)))
+    new_inds = list(set(all_inds) - set(ns_inds))
+
+    lt = {"lables": main[new_inds].tolist()}
+    lt_sk = {"lables": main[ns_inds].tolist()}
+
+    with open(os.path.join(pwd, "dataset_lt.json"), "w") as f:
+        json.dump(lt, f)
+
+    with open(os.path.join(pwd, "dataset_lt_sk.json"), "w") as f:
+        json.dump(lt_sk, f)
+
+
